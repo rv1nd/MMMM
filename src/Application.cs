e6bf8771d5d892dcf9c9ca826e7bb7aca2866c56
Dante Marshal <Marshal.Devilhunter@Gmail.com>
2019-10-07 10:28:07 +0330

    Initial Commit

---
+++ src/Application.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DanteMarshal.Utilities.Telegram.ContactsJsonToVCardConverter
+{
+    static class Application
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+        }
+    }
+}
